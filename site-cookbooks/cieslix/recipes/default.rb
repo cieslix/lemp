55231f854dcf4ea6c597e588aec08dbefcc75bc7
Pawel Cieslik <p.cieslik@macopedia.pl>
2014-11-09 11:45:39 +0100

    add magento, add vhost config for magento

--- site-cookbooks/cieslix/recipes/default.rb
+++ site-cookbooks/cieslix/recipes/default.rb
@@ -1,29 +1,25 @@
-directory node.app.web_dir do
-  owner node.user.name
-  group node.user.group
-  mode "0777"
-  recursive true
+directory "#{node.app.web_dir}/htdocs/" do
+    owner node.user.name
+    group node.user.group
+    mode "0777"
+    recursive true
 end
-
-cookbook_file "#{node.app.web_dir}/index.php" do
-  source "index.php"
-  mode "0777"
-  owner node.user.name
-  group node.user.group
-end
-
-# php_fpm_pool "www"
 
 cieslix_nginx "/etc/nginx/sites-available/#{node.app.name}" do
     # socket "/var/run/php-fpm-www.sock"
     inet_socket "127.0.0.1:8000"
-    root "#{node.app.web_dir}"
-    fastcgi_index "index.php"
-    index "index.html index.htm index.php"
+    root "#{node.app.web_dir}/htdocs"
+    fastcgi_read_timeout "600"
+    # fastcgi_index "index.php"
+    index "index.php index.htm index.html"
     fastcgi_param [
         {
             :name => 'SCRIPT_FILENAME',
             :value => '$document_root$fastcgi_script_name',
+        },
+        {
+            :name => 'MAGE_RUN_CODE',
+            :value => 'default',
         },
         {
             :name => 'SCRIPT_NAME',
@@ -69,5 +65,20 @@ end
 
 execute "create database" do
   action :run
-  command "echo 'CREATE DATABASE IF NOT EXISTS magento CHARSET UTF8' | mysql -uroot -ppassword"
+  command "echo 'CREATE DATABASE IF NOT EXISTS magento CHARSET UTF8' | mysql -uroot -p#{node.mariadb.server_root_password}"
+end
+
+execute "rsync magento" do
+  action :run
+  command "rsync -ra /vagrant/magento/* #{node.app.web_dir}"
+end
+
+execute "add write permissions" do
+  action :run
+  command "chmod a+w #{node.app.web_dir} -R"
+end
+
+execute "change owner" do
+  action :run
+  command "chown #{node.user.name}:#{node.user.group} #{node.app.web_dir} -R"
 end
